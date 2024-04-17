0b0d072d23f742a63c5a0d57a4482f04bc807b8f
@hexlet/cli <support@hexlet.io>
2024-04-17 20:05:30 +0300

    submit rails-basics-ru/crud

--- rails-basics-ru/crud/test/controllers/tasks_controller_test.rb
+++ rails-basics-ru/crud/test/controllers/tasks_controller_test.rb
@@ -1,7 +1,28 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @task = tasks(:one)
+  end
+
+  test 'should get index' do
+    get tasks_path
+    assert_response :success
+  end
+
+  test 'opens all bulletins page' do
+    get root_url
+    assert_response :success
+    assert_select 'h1', 'Tasks'
+  end
+
+  test 'opens one task page with fixture' do
+    task = tasks(:one)
+    get task_path(task)
+    assert_response :success
+    assert_select 'h1', 'Show Task'
+    assert_select 'p', 'Название MyString'
+  end
 end
