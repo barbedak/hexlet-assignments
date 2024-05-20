9c149ff8f5209aea53a3295d06654e4fbe75a710
@hexlet/cli <support@hexlet.io>
2024-05-20 10:28:49 +0300

    submit rails-basics-ru/nested-resources

--- rails-basics-ru/nested-resources/db/schema.rb
+++ rails-basics-ru/nested-resources/db/schema.rb
@@ -10,21 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_517_075_539) do
-  create_table 'post_comments', force: :cascade do |t|
-    t.string 'content'
-    t.integer 'post_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['post_id'], name: 'index_post_comments_on_post_id'
+ActiveRecord::Schema[7.1].define(version: 2024_05_17_075539) do
+  create_table "post_comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_comments_on_post_id"
   end
 
-  create_table 'posts', force: :cascade do |t|
-    t.string 'title', null: false
-    t.text 'body'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'post_comments', 'posts'
+  add_foreign_key "post_comments", "posts"
 end
