4487f91dc821de73f3a394478c4edc3f3a901968
@hexlet/cli <support@hexlet.io>
2024-05-21 15:17:16 +0300

    submit rails-basics-ru/i18n

--- rails-basics-ru/i18n/config/application.rb
+++ rails-basics-ru/i18n/config/application.rb
@@ -20,6 +20,5 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.yml').to_s]
   end
 end
--- rails-basics-ru/i18n/config/initializers/locale.rb
+++ rails-basics-ru/i18n/config/initializers/locale.rb
@@ -4,4 +4,4 @@
 I18n.available_locales = %i[en ru]
 
 # Set default locale to something other than :en
-I18n.default_locale = :en
+I18n.default_locale = :ru
