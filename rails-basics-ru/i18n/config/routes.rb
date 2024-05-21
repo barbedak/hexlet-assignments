6e9eb1b64b887c7fda11b3c922e77315eff09232
@hexlet/cli <support@hexlet.io>
2024-05-21 10:53:47 +0300

    submit rails-basics-ru/i18n

--- rails-basics-ru/i18n/config/routes.rb
+++ rails-basics-ru/i18n/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  scope '/(:locale)', locale: /en|ru/ do
+  scope '(:locale)', locale: /en|ru/ do
     root 'home#index'
     resources :posts do
       scope module: :posts do
