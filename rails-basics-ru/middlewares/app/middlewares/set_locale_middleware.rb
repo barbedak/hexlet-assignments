9ba6f4d831f1a022f6cf9277d226abd913e46f76
@hexlet/cli <support@hexlet.io>
2024-05-27 07:42:21 +0300

    submit rails-basics-ru/middlewares

--- rails-basics-ru/middlewares/app/middlewares/set_locale_middleware.rb
+++ rails-basics-ru/middlewares/app/middlewares/set_locale_middleware.rb
@@ -11,10 +11,9 @@ class SetLocaleMiddleware
 
   def _call(env)
     @status, @headers, @response = @app.call(env)
-    req = Rack::Request.new(env)
-    locales = req.accept_language
-    I18n.locale = locales.any? ? locales.first[0].to_sym : I18n.default_locale
-    Rails.logger.debug(I18n.locale)
+    request = Rack::Request.new(env)
+    locale = request.env['HTTP_ACCEPT_LANGUAGE']&.scan(/^[a-z]{2}/)&.first
+    I18n.locale = locale || I18n.default_locale
     [@status, @headers, @response]
   end
 end
